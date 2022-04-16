9ce221fb1981bbb012a76064d81c6f73f26c78be
Vladimir Radko <vovka@4vrs.com>
2022-04-16 23:08:11 +0700

    Добавил логику для поиска коммутатора

--- src/сomponents/search/SwitchSearchForm.tsx
+++ src/сomponents/search/SwitchSearchForm.tsx
@@ -1,15 +1,41 @@
 import React from "react";
 import {Button, Form, Input, Space, Row} from "antd";
+import {useHttp} from "../../hooks/http.hook";
+import {useNavigate} from "react-router-dom";
 
 type SwitchSearchFormProps = {
   ipAddress: string,
   portNumber: number | string
 };
 
+type formData = {
+  ip: string,
+  portNumber: string
+}
+
 export const SwitchSearchForm: React.FC<SwitchSearchFormProps> = ({ipAddress, portNumber}) => {
+
+  const { request } = useHttp();
+  const navigate = useNavigate();
+
+  const onFormSubmitHandler = ({ ip, portNumber }: formData) => {
+    let url: string = `/api/search/switch/${ip}`;
+    if (portNumber) {
+      url += `/${portNumber}`;
+    }
+    request.get(url).then(response => {
+      console.log('Search response: ', response.data);
+      if (response.status === 200) {
+        navigate('/home', { state: response.data })
+      }
+    });
+  }
+
   return (
     <div>
-      <Form>
+      <Form
+        onFinish={onFormSubmitHandler}
+      >
         <Form.Item style={{display: 'inline-block', marginRight: '5px'}}>
           <Input placeholder="IP адрес" name="ipAddress" value={ipAddress}/>
         </Form.Item>
